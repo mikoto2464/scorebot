f7b997dc3307f387a41c53bf479c1f42452c064f
mikoto <misaka.mikoto2464@gmail.com>
2026-05-15 23:38:51 +0800

    :rocket: 部署基础环境&脚手架

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::str::FromStr;
+use std::time::Duration;
+use dotenvy::dotenv;
+use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode, SqlitePoolOptions};
+use tracing::error;
+use tracing_subscriber::fmt::init;
+
+#[tokio::main]
+async fn main() {
+    init();
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").unwrap_or("sqlite:yukino.db".to_string());
+    let options = SqliteConnectOptions::from_str(&db_url)
+        .unwrap()
+        .foreign_keys(true)
+        .journal_mode(SqliteJournalMode::Wal)
+        .busy_timeout(Duration::from_secs(5));
+    let pool = SqlitePoolOptions::new()
+        .connect_with(options)
+        .await
+        .map_err(|e| {
+            error!("Failed to connect to SQLite database: {}", e);
+        }).unwrap();
 }
