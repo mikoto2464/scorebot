c2c0f2b16ae2732884e48c76175125781f394c58
mikoto <misaka.mikoto2464@gmail.com>
2026-05-16 23:11:16 +0800

    :sparkles: 前端套接字连接

--- scorebot-frontend/src/main.rs
+++ scorebot-frontend/src/main.rs
@@ -1,4 +1,15 @@
+use dotenvy::dotenv;
+use interprocess::local_socket::tokio::prelude::LocalSocketStream;
+use interprocess::local_socket::traits::tokio::Stream;
+use scorebot_common::SOCKET_NAME;
+use tracing_subscriber::fmt::init;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    init();
+    dotenv().ok();
+    let socket_name = SOCKET_NAME.to_fs_name_if_supported()?;
+
+    let mut stream = LocalSocketStream::connect(socket_name).await
+        .expect("Could not connect to socket");
 }
