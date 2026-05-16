089cb077d5cfd9f4cbc3473fa2ce6d3d13509791
mikoto <misaka.mikoto2464@gmail.com>
2026-05-16 23:11:36 +0800

    :sparkles: 通信模型

--- scorebot-common/src/lib.rs
+++ scorebot-common/src/lib.rs
@@ -1,1 +1,20 @@
-pub enum Command {}
+use serde::{Deserialize, Serialize};
+
+pub const SOCKET_NAME: &str = "scorebot.socket";
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Message {
+    pub channel: Channel,
+    pub command: Command
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Channel {
+    OfficialPrivateMessage { openid: String, event_id: String },
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Command {
+    Quit,
+    Help
+}
